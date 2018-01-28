2c16a4654f66bd86c0d4a5271670e0c4939f1af6
jimitgandhi <jimitgandhi@outlook.com>
2018-01-28 09:19:47 +0530

    2
    2

---
+++ app/src/main/java/com/droids/user/examinationcentre/Teacher2Activity.java
@@ -0,0 +1,13 @@
+package com.droids.user.examinationcentre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teacher2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher2);
+    }
+}
